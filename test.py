2cb152625f303a6c9c496312548a7370e4206296
slayer321 <sachin.maurya@fluxninja.com>
2022-12-05 12:10:53 +0530

    add warning

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import logging
 def work():
     for i in range(1,100):
         logging.info("taking info logs")
-        logging.trace("taking trace logs")
+        logging.warning("taking trace logs")
         print(i)
         
         
