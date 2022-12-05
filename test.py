682b0657227774da2debcb741982640b31037897
slayer321 <sachin.maurya@fluxninja.com>
2022-12-05 12:05:59 +0530

    checking for logs

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
+from loguru import logger
+
+
 def work():
     for i in range(1,100):
+        logger.trace("taking trace logs")
+        logger.info("taking info logs")
         print(i)
         
         
