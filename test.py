500a2a2a7ca62fdda8733b7bb082f07fe9964ea3
slayer321 <sachin.maurya@fluxninja.com>
2022-12-05 12:27:03 +0530

    checking for all condition

--- test.py
+++ test.py
@@ -4,8 +4,14 @@ from loguru import logger
 def work():
     for i in range(1,100):
         logger.info("taking info logs")
-        logger.trace("taking trace logs")
+        logger.trace(f"taking trace logs{test}")
+        logger.debug("taking debug logs")
+        logger.warning("taking warning")
         print(i)
-        
+    print("ending")
+    logger.info("outside logger")   
+
+def test():
+    print("test trace file")
         
 work()
