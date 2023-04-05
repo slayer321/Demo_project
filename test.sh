f3d05a268f1f7effcd5812c203de7ee33cf42b50
slayer321 <sachin.maurya@fluxninja.com>
2023-04-05 15:10:00 +0530

    testing docker env parameter

--- test.sh
+++ test.sh
@@ -1,4 +1,13 @@
-BUILD_ENV_ARG="${BUILD_ENV_ARG},BRANCH=main,COMMIT=312322fsd"
+
+echo 'Before addiation ==>'\'"${BUILD_ENV_ARG}"\'
+
+BUILD_ENV_ARG="--build-arg="${BUILD_ENV_ARG}" --build-arg=VERSION="${version}" --build-arg=GIT_BRANCH="${git_branch}" --build-arg=GIT_COMMIT_HASH="${git_commit_hash}""
+
+echo 'After addition ==>'\'$BUILD_ENV_ARG\'
+
+extra_build_args="$(eval echo "$BUILD_ENV_ARG")"
+
+echo 'Print extra_build_args ==>'$extra_build_args
 
 
-echo $BUILD_ENV_ARG
+
